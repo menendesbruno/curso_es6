5865e5ef479715b6252eb36391a17d70d95eda09
Bruno Menendes Rodrigues <menendesbruno@gmail.com>
2020-08-13 20:34:01 -0300

    Adicionando Arrow Functions

--- bundle.js
+++ bundle.js
@@ -29,3 +29,66 @@ var Minhalista = new Todolist();
 document.getElementById('novotodo').onclick = function () {
   Minhalista.addTodo();
 };
+
+var usuario = {
+  nome: 'Diego'
+};
+usuario.nome = 'Cleiton';
+
+function teste(x) {
+  var y = 2;
+  if (x > 5) ;
+  {
+    console.log(x, y);
+  }
+}
+
+teste(10);
+var arr = [1, 3, 4, 5, 8, 9];
+var newarr = arr.map(function (item) {
+  return item * 2;
+});
+console.log(newarr);
+var filter = arr.filter(function (item) {
+  return item % 2 == 0;
+});
+console.log(filter);
+var find = arr.find(function (item) {
+  return item == 4;
+});
+console.log(find);
+var newArr = arr.map(function (item) {
+  return item * 2;
+});
+console.log(newArr);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
+
+var soma = function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
+};
+
+console.log(soma(1));
+console.log(soma());
+console.log(soma(1, 2));
+var arrusuario = {
+  nome: 'Diego',
+  idade: 23,
+  endereço: {
+    cidade: 'Rio do Sul',
+    estado: 'SC'
+  }
+};
+var nome = arrusuario.nome,
+    idade = arrusuario.idade,
+    _arrusuario$endereO = arrusuario.endereço,
+    cidade = _arrusuario$endereO.cidade,
+    estado = _arrusuario$endereO.estado;
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+console.log(estado);
--- main.js
+++ main.js
@@ -11,3 +11,60 @@ const Minhalista = new Todolist();
 document.getElementById('novotodo').onclick = function(){
     Minhalista.addTodo();
 }
+const usuario = { nome: 'Diego' };
+
+usuario.nome = 'Cleiton';
+
+function teste(x) {
+    let y = 2;
+
+    if (x>5); {
+        console.log(x,y);
+    }
+}
+teste(10);
+
+const arr = [1, 3, 4, 5, 8, 9];
+
+const newarr = arr.map(item => item * 2);
+
+console.log(newarr);
+
+const filter = arr.filter(item => item % 2 == 0);
+
+console.log(filter);
+
+const find = arr.find(item => item == 4);
+
+console.log(find);
+
+const newArr = arr.map (item => item * 2);
+
+console.log(newArr);
+
+const sum = arr.reduce(function(total, next) { 
+    return total + next
+});
+
+console.log(sum);
+
+const soma = (a =3, b =6) => a + b;
+console.log(soma(1));
+console.log(soma());
+console.log(soma(1,2));
+
+const arrusuario = {
+    nome: 'Diego',
+    idade: 23,
+    endereço: {
+        cidade: 'Rio do Sul',
+        estado: 'SC',
+    },
+};
+
+const {nome, idade, endereço: {cidade, estado}} = arrusuario;
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+console.log(estado);
